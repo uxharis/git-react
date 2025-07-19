7f38b25ed7897cc96ff8006646be18da5c564dfd
uxharis <uxharis@gmail.com>
2025-07-19 12:31:56 +0530

    Footer ad kardiya hai frontend pe

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg"
 import viteLogo from "/vite.svg"
 import "./App.css"
 import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -26,6 +27,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <Footer />
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react"
 function Header() {
   return (
     <header>
-      <h1 class="text-3xl font-bold underline">Mai header bana raha hu: Haris</h1>
+      <h1 class="text-3xl font-bold">Mai header bana raha hu: Haris</h1>
     </header>
   )
 }
