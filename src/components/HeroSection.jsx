b450dc4c4c5963a146c20fa7f24dd36bef2b6269
uxharis <uxharis@gmail.com>
2025-07-19 12:49:43 +0530

    hero section layout added

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+function HeroSection() {
+  return (
+    <section>
+      <div className="container mx-auto px-4">
+        <h2 className="text-2xl font-bold">Hero Section Ban gaya hai</h2>
+      </div>
+    </section>
+  )
+}
+
+export default HeroSection
