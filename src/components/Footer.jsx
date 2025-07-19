e0b905d06d30114ba11e5667df1647617a10d7ef
Haris Akbar <uxharis@gmail.com>
2025-07-19 14:00:57 +0530

    Revert "New udated on footer by junior dev"

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,6 @@ function Footer() {
   return (
     <div className="container mx-auto px-4">
       <h1 class="text-2xl font-bold">Footer ban gaya hai: Junior dev(hfwork)</h1>
-      <p>New update on footer</p>
     </div>
   )
 }
