f2928a2ac6593cb1c694b5469f59e4d0b54c6781
uxharis <uxharis@gmail.com>
2025-07-19 13:05:30 +0530

    hero section mein naye features add kiye

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 function HeroSection() {
   return (
-    <section>
+    <section className="bg-gray-300 py-10 text-center">
       <div className="container mx-auto px-4">
         <h2 className="text-2xl font-bold">Hero Section Ban gaya hai</h2>
       </div>
