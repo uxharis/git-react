10ab1901beb7758af98c4c5e83b27c167f3d692c
uxharis <uxharis@gmail.com>
2025-07-19 13:00:20 +0530

    boilerplate code removed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,11 @@
 import React from "react"
 
 function Footer() {
-  return <div>
-    <h1 class="text-2xl font-bold">Footer ban gaya hai: Junior dev(hfwork)</h1>
-  </div>
+  return (
+    <div className="container mx-auto px-4">
+      <h1 class="text-2xl font-bold">Footer ban gaya hai: Junior dev(hfwork)</h1>
+    </div>
+  )
 }
 
 export default Footer
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 function Header() {
   return (
-    <header>
+    <header className="container mx-auto px-4 mt-5">
       <h1 class="text-3xl font-bold">Mai header bana raha hu: Haris</h1>
     </header>
   )
