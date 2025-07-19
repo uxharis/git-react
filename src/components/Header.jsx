4b60f349bfe752c195a1a4b84e9bca70c473f22e
uxharis <uxharis@gmail.com>
2025-07-19 11:48:25 +0530

    Tailwind Installed ho gaya hai

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Header() {
+  return (
+    <header>
+      <h1 class="text-3xl font-bold underline">Mai header bana raha hu: Haris</h1>
+    </header>
+  )
+}
+
+export default Header
