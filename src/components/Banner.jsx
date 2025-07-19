c750a5131e65efa30c4acb74d905170b5fef9921
uxharis <uxharis@gmail.com>
2025-07-19 12:16:38 +0530

    Code for junior dev: hfwork

---
+++ src/components/Banner.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Banner() {
+  return (
+    <section className="banner">
+      <div className="container mx-auto px-4 ">
+        <h3>Banner yaha banana</h3>
+      </div>
+    </section>
+  )
+}
+
+export default Banner
